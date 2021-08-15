7479f4df096b135116144f01c050913949264a98
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-08-15 02:46:38 -0300

    Tag for elements to be placed first (used by KaiOS header)

--- platform-kaios.js
+++ platform-kaios.js
@@ -2,7 +2,16 @@ document.querySelector("#head").innerHTML += `<link rel="stylesheet" type="text/
 document.querySelector("#head").innerHTML += `<link rel="stylesheet" type="text/css" href="css/header.css">`;
 document.querySelector("#head").innerHTML += `<link rel="stylesheet" type="text/css" href="css/softkey.css">`;
 
-//document.querySelector("#body").innerHTML += `<header><span>FrameworkJS r3</span></header>`;
-document.querySelector("#body").innerHTML += `<style>.fm-header p{line-height: var(--header-height);background: var(--header-background);color: var(--color-gs00);font-weight: 300;padding: 0 1rem;text-align: center;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;margin: 0;text-transform: none;}</style>`;
-document.querySelector("#body").innerHTML += `<div class="statusbar-placeholder"></div><header class="fm-header h1"><span>FrameworkJS r3</span></header>`;
+document.querySelector("#placefirst").innerHTML += `<header><span>FrameworkJS r3</span></header>`;
+
+//var insertheaderfirst = document.getElementById('body');
+//insertheaderfirst.insertBefore(document.createElement("header"), container.firstChild);
+//insertheaderfirst.insertBefore(document.TextNode("<header><span>FrameworkJS r3</span></header>"), container.firstChild);
+// This type of JS isn't supported on KaiOS
+
+//document.querySelector("#body").innerHTML += `<div class="statusbar-placeholder"></div><header class="fm-header h1"><span>FrameworkJS r3</span></header>`;
 document.querySelector("#body").innerHTML += `<footer class="softkey"><div id="softkey-left">Back</div><div id="softkey-center">RELOAD</div><div id="softkey-right">More</div></footer>`;
+
+document.querySelector("#body").innerHTML += `<style>h1{font-size:25px}span{font-size:12px}h2{font-size:20px}</style>`;
+document.querySelector("#landinglogo").innerHTML = `<img width="50px" src="logo.svg"/>`;
+document.querySelector("#landingtitle").innerHTML = ``;
