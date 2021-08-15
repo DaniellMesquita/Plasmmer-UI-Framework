ea67fa30668306a944e70283e0fc7a17d6224257
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-08-14 22:38:09 -0300

    Detect Firefox OS

---
+++ index.js
@@ -0,0 +1,68 @@
+window.onload = function(){
+
+if("file:" == document.location.protocol)
+	{
+                console.log("Environment detected: File protocol; Local page");
+                //document.querySelector("#environment").innerHTML = `File protocol; Local page`;
+	}
+
+if("localhost:" == document.location.protocol)
+	{
+                console.log("Environment detected: Localhost");
+                //document.querySelector("#environment").innerHTML = `Localhost`;
+	}
+
+if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+    {
+        alert('Opera');
+    }
+    else if(navigator.userAgent.indexOf("Chrome") != -1 )
+    {
+        alert('Chrome');
+        /* document.querySelector("#environment").innerHTML = `Chrome`; */
+    }
+    else if(navigator.userAgent.indexOf("Safari") != -1)
+    {
+        alert('Safari');
+    }
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+    {
+         //alert('Firefox');
+    }
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+    {
+      alert('IE'); 
+    }  
+    else if(navigator.userAgent.indexOf("KAIOS") != -1 ) 
+    {
+         console.log("Environment detected: KaiOS.");
+         //document.querySelector("#environment").innerHTML = `KaiOS`;
+    }
+    else 
+    {
+       alert('unknown');
+    }
+
+//function setInnerHTML(element, content) {
+//    element.innerHTML = content;
+//    return element;
+//}
+
+var OSName="Unknown OS";
+if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+if (navigator.userAgent.indexOf("Mozilla/5.0 (Mobile; rv")!=-1) OSName="Firefox OS";
+//if (navigator.appVersion.indexOf("KAIOS")!=-1) OSName="KaiOS";
+console.log('Your OS: '+OSName);
+//alert('Your OS: '+OSName);
+
+if(OSName == 'Firefox OS'){
+        var environmenttext = document.getElementById('environment');
+        var text = document.createTextNode("Firefox OS");
+        environmenttext.appendChild(text);
+        //document.getElementsByTagName('body')[0].appendChild(setInnerHTML(document.createElement("div"), "Firefox OS"));
+}
+
+}
